6e5cda1e39852217171c3bac46aaed5ad5e3a3a1
Manu <josecastyyo16@gmail.com>
2024-11-25 17:41:10 -0600

    Co-authored-by: LUIS DANIEL FAJARDO SALAIZA <2123300397@utj.edu.mx>

--- resources/views/logeado/overview.blade.php
+++ resources/views/logeado/overview.blade.php
@@ -254,5 +254,50 @@
     <p class="mb-3 text-black-500 dark:text-black-400">¡Muy recomendado!
     </p>
 </article>
+<article style="background-color: #f4f4f4; margin: 3rem; border-radius: 7px; padding: 2rem;">
+    <div class="flex items-center mb-4">
+        <img class="w-10 h-10 me-4 rounded-full" src="{{ url('/storage/chris.jpg') }}" alt="Jese">
+        <div class="font-medium dark:text-black">
+            <p>Nelida Abril <time datetime="2014-08-16 19:00" class="block text-sm text-black-500 dark:text-black-400">
+                    Fecha de la visita Agosto 2015</time>
+            </p>
+        </div>
+    </div>
+    <div class="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
+        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+            viewBox="0 0 22 20">
+            <path
+                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+            viewBox="0 0 22 20">
+            <path
+                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+            viewBox="0 0 22 20">
+            <path
+                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+            viewBox="0 0 22 20">
+            <path
+                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+            viewBox="0 0 22 20">
+            <path
+                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+        </svg>
+        <h3 class="ms-2 text-sm font-semibold text-gray-900 dark:text-black">Sumamente delicioso y claro que volvere!</h3>
+    </div>
+    <p class="mb-2 text-black-500 dark:text-black-400">La Fonda San Francisco es mi lugar favorito para desayunar chilaquiles. 
+        La salsa tiene el nivel perfecto de picante y las porciones son generosas. 
+        Además, los tacos al pastor son de los mejores que he probado: la carne está jugosa y bien sazonada. 
+        ¡Siempre regreso por el excelente servicio y la comida deliciosa!
+    </p>
+    <p class="mb-3 text-black-500 dark:text-black-400">Excelente lugar, muy recomendado, visitenlos no se arrepentiran!.
+    </p>
+</article>
 
 @include('/plantillas/footer')
